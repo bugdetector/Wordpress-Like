add0021220f4dbbbf18ff059a753651831b299d6
bugdetector <bakiyucel38@gmail.com>
2017-04-02 00:10:19 +0300

    Create functions.js

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+function deneme(button) {
+	var id = button.id.replace( /^\D+/g, '');
+	var text =  document.getElementById("text-"+button.id);
+
+    jQuery.ajax({
+        type : "post",
+        dataType : "json",
+        url : ajaxElement.ajaxurl,
+        data : {action: "like_button_clicked",id:id,state:text.innerHTML},
+        success: function(response) {
+            text.innerHTML = response.state == 1 ? "Beğenildi":"Beğenilmedi" ;
+         }
+    });
+}
